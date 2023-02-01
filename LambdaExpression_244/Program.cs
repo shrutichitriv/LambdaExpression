6ea8a7c26fd4d257b152d2cc542785dfb1d19950
shrutichitriv <shrutichitriv199620@gmail.com>
2023-02-01 16:09:26 +0530

    [Shruti] Perform Lambda expression program

---
+++ LambdaExpression_244/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace LambdaExpression_244
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Lambda Expression Program");
+
+        }
+    }
+}
